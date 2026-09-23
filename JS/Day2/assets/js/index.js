8d82ca0f29ac651491d703ff076e23a4a677de8a
unknown <catherine>
2026-09-23 14:58:35 +0530

    updated code

---
+++ JS/Day2/assets/js/index.js
@@ -0,0 +1,281 @@
+var userId
+console.log(userId);
+
+var classRoom
+console.log(classRoom);
+
+var remoteCar
+console.log( remoteCar);
+
+var childHood
+console.log(childHood);
+
+var dailyTask
+console.log(dailyTask);
+
+var userName=10+20
+console.log(10+20);
+
+var userId=10+10
+console.log(20+20);
+
+var indexJs=10+10+10
+console.log(10+25+20);
+
+ var controllRoom=2+11
+ console.log(22+38);
+
+ var userId=2+3+4+5+6+7+8+9
+console.log(3+4+5+6+7+8+9);
+
+var name = "Catherine";
+console.log(name);
+
+var city = "Chennai";
+console.log(city);
+
+var course = "Full Stack Development";
+console.log(course);
+
+var language = "JavaScript";
+console.log(language);
+
+var college = "Anna University";
+console.log(college);
+
+var fruit = "Apple";
+console.log(fruit);
+
+var color = "Blue";
+console.log(color);
+
+var country = "India";
+console.log(country);
+
+var hobby = "Music";
+console.log(hobby);
+
+var greeting = "Hello";
+console.log(greeting);
+
+var age = 25;
+console.log(age);
+
+var marks = 90;
+console.log(marks);
+
+var price = 500;
+console.log(price);
+
+var quantity = 10;
+console.log(quantity);
+
+var number1 = 100;
+console.log(number1);
+
+var number2 = 25.5;
+console.log(number2);
+
+var salary = 25000;
+console.log(salary);
+
+var score = 95;
+console.log(score);
+
+var year = 2026;
+console.log(year);
+
+var total = 1000;
+console.log(total);
+
+var user = null;
+console.log(user);
+
+var address = null;
+console.log(address);
+
+var phone = null;
+console.log(phone);
+
+var email = null;
+console.log(email);
+
+var product = null;
+console.log(product);
+
+var result = null;
+console.log(result);
+
+var account = null;
+console.log(account);
+
+var image = null;
+console.log(image);
+
+var message = null;
+console.log(message);
+
+var value = null;
+console.log(value);
+
+
+
+
+var a;
+console.log(a);
+
+var b;
+console.log(b);
+
+var c;
+console.log(c);
+
+var d;
+console.log(d);
+
+var e;
+console.log(e);
+
+var f;
+console.log(f);
+
+var g;
+console.log(g);
+
+var h;
+console.log(h);
+
+var i;
+console.log(i);
+
+var j;
+console.log(j);
+
+var isStudent = true;
+console.log(isStudent);
+
+var isWorking = false;
+console.log(isWorking);
+
+var isHappy = true;
+console.log(isHappy);
+
+var isCoding = false;
+console.log(isCoding);
+
+var isPresent = true;
+console.log(isPresent);
+
+
+let studentName = "Arun";
+console.log(studentName);
+
+let state = "Tamil Nadu";
+console.log(state);
+
+let subject = "Mathematics";
+console.log(subject);
+
+let vehicle = "Car";
+console.log(vehicle);
+
+let temperature = 32;
+console.log(temperature);
+
+let mobileNumber = 9876543210;
+console.log(mobileNumber);
+
+let isOnline = true;
+console.log(isOnline);
+
+let paymentStatus = false;
+console.log(paymentStatus);
+
+let employeeRole = "Developer";
+console.log(employeeRole);
+
+let projectCount = 5;
+console.log(projectCount);
+
+// let value can be changed
+let currentCity = "Chennai";
+console.log(currentCity);
+
+currentCity = "Bangalore";
+console.log(currentCity);
+
+let monthlyExpense = 15000;
+console.log(monthlyExpense);
+
+monthlyExpense = 18000;
+console.log(monthlyExpense);
+
+const companyName = "Tech Solutions";
+console.log(companyName);
+
+const countryCode = "+91";
+console.log(countryCode);
+
+const birthYear = 2000;
+console.log(birthYear);
+
+const piValue = 3.14;
+console.log(piValue);
+
+const maximumMarks = 100;
+console.log(maximumMarks);
+
+const websiteName = "Google";
+console.log(websiteName);
+
+const laptopBrand = "Dell";
+console.log(laptopBrand);
+
+const officeLocation = "Chennai";
+console.log(officeLocation);
+
+const accountBalance = 25000;
+console.log(accountBalance);
+
+const openingTime = "09:00 AM";
+console.log(openingTime);
+
+const isVerified = true;
+console.log(isVerified);
+
+const emptyData = null;
+console.log(emptyData);
+
+
+
+let firstValue = 20;
+let secondValue = 30;
+let addition = firstValue + secondValue;
+console.log(addition);
+
+let length = 10;
+let width = 5;
+let rectangleArea = length * width;
+console.log(rectangleArea);
+
+let totalAmount = 1000;
+let discount = 100;
+let finalAmount = totalAmount - discount;
+console.log(finalAmount);
+
+
+
+const firstNumber = 12;
+const secondNumber = 4;
+const divisionResult = firstNumber / secondNumber;
+console.log(divisionResult);
+
+const itemPrice = 250;
+const itemQuantity = 3;
+const billAmount = itemPrice * itemQuantity;
+console.log(billAmount);
+
+const tamilMark = 85;
+const englishMark = 90;
+const totalMark = tamilMark + englishMark;
+console.log(totalMark);
